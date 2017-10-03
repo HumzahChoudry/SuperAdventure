0936aeb10ae8f0aeeb787852b6987b11904e7cd8
HumzahChoudry <Humzah.Choudry@gmail.com>
2017-10-03 13:04:11 -0400

    first commit
    initial commit of completed game

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    //To serve as base class for monsters/players
+    public class LivingCreature
+    {
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+
+        public LivingCreature(int currentHitPoints, int maximumHitPoints)
+        {
+            CurrentHitPoints = currentHitPoints;
+            MaximumHitPoints = maximumHitPoints;
+        }
+
+    }
+
+
+
+}
